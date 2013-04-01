20d874fde07768338a3385a53ebf77eca0751bdd
saldanha.plangeo@gmail.com <saldanha.plangeo@gmail.com@bb25ec27-eb88-c4e7-cec9-705dc4859ef8>
2013-04-01 15:16:03 +0000

    Calculando distâncias!

--- trunk/memorial/Form1.Designer.cs
+++ trunk/memorial/Form1.Designer.cs
@@ -40,16 +40,16 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(724, 249);
+            this.dataGridView1.Size = new System.Drawing.Size(769, 249);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 267);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(115, 52);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Abrir arquivo CSV";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -59,11 +59,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 267);
+            this.button2.Location = new System.Drawing.Point(133, 267);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(153, 52);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Calcular distância entre pontos";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -71,7 +71,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(748, 302);
+            this.ClientSize = new System.Drawing.Size(793, 516);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
