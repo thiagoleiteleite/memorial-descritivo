c6120e7db4e6d6b1bf051499e93e407328df7bac
saldanha.plangeo@gmail.com <saldanha.plangeo@gmail.com@bb25ec27-eb88-c4e7-cec9-705dc4859ef8>
2013-04-01 19:48:23 +0000

    Cálculo do azimute pronto!

--- trunk/memorial/Form1.cs
+++ trunk/memorial/Form1.cs
@@ -61,22 +61,50 @@ namespace memorial
         private void button2_Click(object sender, EventArgs e)
         {
             dt2.Columns.Add(new DataColumn("Distância", typeof(decimal)));
+            dt2.Columns.Add(new DataColumn("Azimute", typeof(decimal)));
 
             for (int i = 1; i < dt2.Rows.Count; i++)
             {
+                //Cálculo da distância
                 double X = Convert.ToDouble(dt2.Rows[i]["X"]);
                 double Xant = Convert.ToDouble(dt2.Rows[i-1]["X"]);
                 double difX = X - Xant;
                 double Y = Convert.ToDouble(dt2.Rows[i]["Y"]);
                 double Yant = Convert.ToDouble(dt2.Rows[i - 1]["Y"]);
-                double difY = Y - Yant;
-                dt2.Rows[i]["Distância"] =  Math.Sqrt(Math.Pow(difX,2)+Math.Pow(difY,2));
+                double difY = Y - Yant;                
+                dt2.Rows[i]["Distância"] = Math.Sqrt(Math.Pow(difX, 2) + Math.Pow(difY, 2));
 
+                //Cálculo do azimute
                 double rprov1 = Math.Atan(difX / difY) * (180/Math.PI) * Math.Sign(difX/difY);
+                double azprov1 = rprov1;
+                double azprov2 = 180 - rprov1;
+                double azprov3 = 180 + rprov1;
+                double azprov4 = 360 - rprov1;
+                double azprov1b = ((Math.Truncate(azprov1 * Math.Sign(azprov1)) + (Math.Truncate((azprov1 - (Math.Truncate(azprov1 * Math.Sign(azprov1)) * Math.Sign(azprov1))) * 60 * Math.Sign(azprov1))) / 100) + (((azprov1 - (Math.Truncate(azprov1 * Math.Sign(azprov1)) * Math.Sign(azprov1))) * 60 * Math.Sign(azprov1) - Math.Truncate((azprov1 - (Math.Truncate(azprov1 * Math.Sign(azprov1)) * Math.Sign(azprov1))) * 60 * Math.Sign(azprov1))) * 60 / 10000)) * Math.Sign(azprov1);
+                double azprov2b = ((Math.Truncate(azprov2 * Math.Sign(azprov2)) + (Math.Truncate((azprov2 - (Math.Truncate(azprov2 * Math.Sign(azprov2)) * Math.Sign(azprov2))) * 60 * Math.Sign(azprov2))) / 100) + (((azprov2 - (Math.Truncate(azprov2 * Math.Sign(azprov2)) * Math.Sign(azprov2))) * 60 * Math.Sign(azprov2) - Math.Truncate((azprov2 - (Math.Truncate(azprov2 * Math.Sign(azprov2)) * Math.Sign(azprov2))) * 60 * Math.Sign(azprov2))) * 60 / 10000)) * Math.Sign(azprov2);
+                double azprov3b = ((Math.Truncate(azprov3 * Math.Sign(azprov3)) + (Math.Truncate((azprov3 - (Math.Truncate(azprov3 * Math.Sign(azprov3)) * Math.Sign(azprov3))) * 60 * Math.Sign(azprov3))) / 100) + (((azprov3 - (Math.Truncate(azprov3 * Math.Sign(azprov3)) * Math.Sign(azprov3))) * 60 * Math.Sign(azprov3) - Math.Truncate((azprov3 - (Math.Truncate(azprov3 * Math.Sign(azprov3)) * Math.Sign(azprov3))) * 60 * Math.Sign(azprov3))) * 60 / 10000)) * Math.Sign(azprov3);
+                double azprov4b = ((Math.Truncate(azprov4 * Math.Sign(azprov4)) + (Math.Truncate((azprov4 - (Math.Truncate(azprov4 * Math.Sign(azprov4)) * Math.Sign(azprov4))) * 60 * Math.Sign(azprov4))) / 100) + (((azprov4 - (Math.Truncate(azprov4 * Math.Sign(azprov4)) * Math.Sign(azprov4))) * 60 * Math.Sign(azprov4) - Math.Truncate((azprov4 - (Math.Truncate(azprov4 * Math.Sign(azprov4)) * Math.Sign(azprov4))) * 60 * Math.Sign(azprov4))) * 60 / 10000)) * Math.Sign(azprov4);
+                if (difY > 0 && difX > 0)
+                {
+                    dt2.Rows[i]["Azimute"] = azprov1b;
+                }
+                if (difY < 0 && difX > 0)
+                {
+                    dt2.Rows[i]["Azimute"] = azprov2b;
+                }
+                if (difY < 0 && difX < 0)
+                {
+                    dt2.Rows[i]["Azimute"] = azprov3b;
+                }
+                if (difY > 0 && difX < 0)
+                {
+                    dt2.Rows[i]["Azimute"] = azprov4b;
+                }
             }
 
             dataGridView1.DataSource = dt2;            
             dataGridView1.Columns[3].DefaultCellStyle.Format = "0.0000";
+            dataGridView1.Columns[4].DefaultCellStyle.Format = "0° .00´ 00','00´´";
         }
 
     }
