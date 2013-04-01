696dee7a28b47ef580a45ab86338d613e3409b25
saldanha.plangeo@gmail.com <saldanha.plangeo@gmail.com@bb25ec27-eb88-c4e7-cec9-705dc4859ef8>
2013-04-01 12:14:19 +0000

    Lendo arquivos CSV, separados por vírgula simples!

---
+++ trunk/memorial/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
+
+namespace memorial
+{
+    public partial class Form1 : Form
+    {
+        string arquivo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void readCsv(string arquivo)
+        {
+            DataTable dt = new DataTable();
+
+            //Fonte: http://www.codeproject.com/Articles/9258/A-Fast-CSV-Reader            
+            using (CachedCsvReader csv = new
+            //CachedCsvReader(new StreamReader(arquivo), true))
+            CachedCsvReader(new StreamReader(arquivo), true))
+            {
+                dt.Load(csv);
+                dataGridView1.DataSource = dt;
+            }           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                arquivo = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readCsv(arquivo);
+        }
+    }
+}
