a5a26d0e9c6d90c16c79298ee7152af1423dd347
saldanha.plangeo <saldanha.plangeo@gmail.com>
2013-04-02 14:04:01 +0000

    Coisas.

--- memorial/Form1.Designer.cs
+++ memorial/Form1.Designer.cs
@@ -626,7 +626,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(266, 5);
+            this.button5.Location = new System.Drawing.Point(351, 4);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(189, 31);
@@ -637,10 +637,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(135, 5);
+            this.button4.Location = new System.Drawing.Point(173, 5);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(127, 31);
+            this.button4.Size = new System.Drawing.Size(174, 31);
             this.button4.TabIndex = 2;
             this.button4.Text = "Gravar arquivo RTF";
             this.button4.UseVisualStyleBackColor = true;
@@ -651,7 +651,7 @@
             this.button3.Location = new System.Drawing.Point(4, 5);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(127, 31);
+            this.button3.Size = new System.Drawing.Size(165, 31);
             this.button3.TabIndex = 1;
             this.button3.Text = "Gerar memorial";
             this.button3.UseVisualStyleBackColor = true;
@@ -720,7 +720,7 @@
             // 
             this.label21.AutoSize = true;
             this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label21.Location = new System.Drawing.Point(12, 88);
+            this.label21.Location = new System.Drawing.Point(12, 89);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(224, 15);
             this.label21.TabIndex = 2;
